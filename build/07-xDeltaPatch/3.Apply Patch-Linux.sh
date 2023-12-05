1c88d617dae262dac321757166006cbfa3553d13
Risae <37260321+Risae@users.noreply.github.com>
2023-12-05 07:38:55 +0100

    Update

--- build/07-xDeltaPatch/3.Apply Patch-Linux.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-cd "$(cd "$(dirname "$0")" && pwd)"
-mkdir old
-chmod +x xdelta3
-./xdelta3 -v -d -s "Growlanser 6 Precarious World.iso" "vcdiff/Growlanser 6 Precarious World.iso.vcdiff" "Growlanser 6 Precarious World English.ISO"
-mv "Growlanser 6 Precarious World.iso" old
