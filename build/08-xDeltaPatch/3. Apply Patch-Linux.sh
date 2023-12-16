7153260b903245023fbd132a711c71577ca63265
Risae <37260321+Risae@users.noreply.github.com>
2023-12-16 09:41:55 +0100

    Delete 3. Apply Patch-Linux.sh

--- build/08-xDeltaPatch/3. Apply Patch-Linux.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-echo "[i] Starting to patch the ISO."
-cd "$(cd "$(dirname "$0")" && pwd)"
-chmod +x bin/xdelta3
-bin/xdelta3 -v -d -s "Growlanser 6 Precarious World.iso" "vcdiff/Growlanser 6 Precarious World.iso.vcdiff" "Growlanser 6 Precarious World English.iso"
-echo "[i] Completed!"
