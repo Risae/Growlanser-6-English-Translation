40b17eba92359f81370b36943cdac5ed1ec012a0
Risae <37260321+Risae@users.noreply.github.com>
2023-12-16 09:33:05 +0100

    Update 3.Apply Patch-Windows.bat

--- build/08-xDeltaPatch/3.Apply Patch-Windows.bat
+++ build/08-xDeltaPatch/3.Apply Patch-Windows.bat
@@ -1,6 +1,5 @@
 @echo off
-mkdir old
-.\xdelta-3.1.0-x86_64.exe -v -d -s "Growlanser 6 Precarious World.iso" ".\vcdiff\Growlanser 6 Precarious World.iso.vcdiff" "Growlanser 6 Precarious World English.ISO"
-move "Growlanser 6 Precarious World.iso" old
-echo Completed!
+echo "[i] Starting to patch the ISO."
+bin\xdelta-3.1.0-x86_64.exe -v -d -s "Growlanser 6 Precarious World.iso" ".\vcdiff\Growlanser 6 Precarious World.iso.vcdiff" "Growlanser 6 Precarious World English.iso"
+echo "[i] Completed!"
 @pause
