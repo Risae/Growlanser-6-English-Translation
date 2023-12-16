c52e38e5306cc3dbeda70f475a9e3f5e71cdd776
Risae <37260321+Risae@users.noreply.github.com>
2023-12-16 09:41:59 +0100

    Delete 3. Apply Patch-Windows.bat

--- build/08-xDeltaPatch/3. Apply Patch-Windows.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-echo "[i] Starting to patch the ISO."
-bin\xdelta-3.1.0-x86_64.exe -v -d -s "Growlanser 6 Precarious World.iso" ".\vcdiff\Growlanser 6 Precarious World.iso.vcdiff" "Growlanser 6 Precarious World English.iso"
-echo "[i] Completed!"
-@pause
